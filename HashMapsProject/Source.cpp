d2240cc857082b0fbf28f962cadaefd9f9fbea3e
beijie1122 <beijiebabb@gmail.com>
2023-11-13 00:17:52 -0800

    Added remove duplicates function for Maps

--- HashMapsProject/Source.cpp
+++ HashMapsProject/Source.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
+#include <vector>
+
+std::vector<int> RemoveDuplicates(int* arr, int n)
+{
+	std::vector<int> output;
+	std::unordered_map<int, bool> RemoveDupesMap;
+
+	for (size_t i = 0; i < n; i++)
+	{
+		if (RemoveDupesMap.count(arr[i]) == 0)
+		{
+			output.push_back(arr[i]);
+			RemoveDupesMap[arr[i]] = true;
+		}
+		else
+		{
+			std::cout << "Duplicate Found!\n";
+		}
+	}
+
+	return output;
+}
+
 
 int main()
 {
@@ -46,6 +69,13 @@ int main()
 
 	MyMap.erase("GHI");
 
+	int DuplicatesArray[] = { 1, 5 ,2 ,4, 4, 3, 3, 6, 7, 1, 1, 2 };
+	std::vector<int> OutputVector = RemoveDuplicates(DuplicatesArray, 12);
+
+	for (size_t i = 0; i < OutputVector.size(); i++)
+	{
+		std::cout << OutputVector.at(i) << "\n";
+	}
 
 	return 0;
 }
